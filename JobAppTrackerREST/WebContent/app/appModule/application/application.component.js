dffd6ce053edfae4940e4efac540506f7756eee0
Brian Thomas <brianraythomas@gmail.com>
2017-11-12 13:34:49 -0700

    show/hide set for update/new/index/show parts

--- JobAppTrackerREST/WebContent/app/appModule/application/application.component.js
+++ JobAppTrackerREST/WebContent/app/appModule/application/application.component.js
@@ -31,6 +31,7 @@ angular.module('appModule')
 			});
 			
 			vm.setCreateApplication = function(){
+				vm.applicationSelected = null;
 				vm.createApplication = true;
 			};
 /******************************* create new application ***************************************/
