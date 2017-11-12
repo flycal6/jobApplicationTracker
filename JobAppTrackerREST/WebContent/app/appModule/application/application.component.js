3534fd3bbc3e939d05698192625e60e4802a0d16
Brian Thomas <brianraythomas@gmail.com>
2017-11-11 23:16:46 -0700

    index applications shown

--- JobAppTrackerREST/WebContent/app/appModule/application/application.component.js
+++ JobAppTrackerREST/WebContent/app/appModule/application/application.component.js
@@ -1,8 +1,18 @@
 angular.module('appModule')
 	.component('application', {
 		templateUrl: 'app/appModule/application/application.component.html',
-		controller: function(){
+		controller: function(appService){
 			var vm = this;
+			vm.applications = [];
+			
+			var reload = function(){
+				appService.index().then(function(res){
+					vm.applications = res.data;
+					console.log(vm.applications)
+				})
+			};
+			
+			reload();
 		},
 		controllerAs: 'vm'
 	});
