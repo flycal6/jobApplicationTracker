ce54bd283935c777311d8212ba197808072fbb48
Brian Thomas <brianraythomas@gmail.com>
2017-11-11 19:14:24 -0700

    application create method implemented, routed in controller

--- JobAppTrackerREST/src/controllers/ApplicationController.java
+++ JobAppTrackerREST/src/controllers/ApplicationController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,22 +27,24 @@ public class ApplicationController implements ApplicationControllerI {
 	}
 
 	@Override
-	@RequestMapping(path="user/{uid}/app")
+	@RequestMapping(path="user/{uid}/app", method=RequestMethod.GET)
 	public Collection<Application> index(HttpServletRequest req, HttpServletResponse res, @PathVariable int uid) {
-		// TODO Auto-generated method stub
+		res.setStatus(202);
 		return appDao.index(uid);
 	}
 
 	@Override
-	public Application show(HttpServletRequest req, HttpServletResponse res, int uid, int aid) {
-		// TODO Auto-generated method stub
-		return null;
+	@RequestMapping(path="user/{uid}/app/{aid}", method=RequestMethod.GET)
+	public Application show(HttpServletRequest req, HttpServletResponse res, @PathVariable int uid, @PathVariable int aid) {
+		res.setStatus(202);
+		return appDao.show(uid, aid);
 	}
 
 	@Override
-	public Application create(HttpServletRequest req, HttpServletResponse res, int uid, String appJson) {
-		// TODO Auto-generated method stub
-		return null;
+	@RequestMapping(path="user/{uid}/app", method=RequestMethod.POST)
+	public Application create(HttpServletRequest req, HttpServletResponse res, @PathVariable int uid, @RequestBody String appJson) {
+		res.setStatus(201);
+		return appDao.create(uid, appJson);
 	}
 
 	@Override
--- JobAppTrackerREST/src/data/ApplicationDAOImpl.java
+++ JobAppTrackerREST/src/data/ApplicationDAOImpl.java
@@ -10,7 +10,10 @@ import javax.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import entities.Application;
+import entities.User;
 
 @Repository
 @Transactional
@@ -21,7 +24,7 @@ public class ApplicationDAOImpl implements ApplicationDAO {
 
 	@Override
 	public Set<Application> index(int uid) {
-		String q = "SELECT a FROM Application a WHERE id=:uid";
+		String q = "SELECT a FROM Application a WHERE a.user.id = :uid";
 		List<Application> apps = em.createQuery(q, Application.class)
 									.setParameter("uid", uid)
 									.getResultList();
@@ -30,13 +33,29 @@ public class ApplicationDAOImpl implements ApplicationDAO {
 
 	@Override
 	public Application show(int uid, int aid) {
-		// TODO Auto-generated method stub
+		String q = "SELECT a FROM Application a WHERE id=:aid AND a.user.id = :uid";
+		List<Application> apps = em.createQuery(q, Application.class)
+									.setParameter("uid", uid)
+									.setParameter("aid", aid)
+									.getResultList();
+		if(apps.size() > 0) {
+			return apps.get(0);
+		}
 		return null;
 	}
 
 	@Override
 	public Application create(int uid, String appJson) {
-		// TODO Auto-generated method stub
+		ObjectMapper om = new ObjectMapper();
+		try {
+			Application a = om.readValue(appJson, Application.class);
+			a.setUser(em.find(User.class, uid));
+			em.persist(a);
+			em.flush();
+			return a;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
 
