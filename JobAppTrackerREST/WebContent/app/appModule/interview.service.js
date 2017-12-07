a094247b30421edf05c4550c8c25cdf0f38114f3
Brian Thomas <brianraythomas@gmail.com>
2017-12-07 15:50:25 -0700

    service to hold currently selected app/response created

--- JobAppTrackerREST/WebContent/app/appModule/interview.service.js
+++ JobAppTrackerREST/WebContent/app/appModule/interview.service.js
@@ -1,6 +1,9 @@
 angular.module('appModule')
 	.factory('interviewService', function($http, authService, $location){
 		var service = {};
+		service.selectedRes = null;
+		service.selectedApp = null;
+		service.viewIntForm = false;
 		
 		service.index = function(){
 			var uid = authService.getToken().id;
@@ -24,6 +27,12 @@ angular.module('appModule')
 			});
 		};
 		
+		service.storeResAndApp = function(res, app){
+			service.selectedRes = res;
+			service.selectedApp = app;
+			service.viewIntForm = true;
+			$location.path('/interviews');
+		};
 		
 		return service;
 	});
